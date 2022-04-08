3cc8f69a9dbcbb94ea652ab4f10d00b6b7149565
martin-0 <martin@bxlr.sk>
2022-04-08 13:58:46 +0200

    add cmdline options

--- run.sh
+++ run.sh
@@ -1,18 +1,44 @@
-#qemu-system-x86_64 -curses -hda stand.raw
-#qemu-system-x86_64 -curses -monitor stdio mios
+#!/bin/sh
 
 MONITOR_TELNET="-monitor telnet:127.0.0.1:10101,server,nowait"
 MONITOR_STDIO="-monitor stdio"
 
+MONITOR=""
 DEBUG=""
-if [ "z"$1 = "zd" ]; then 
-	DEBUG="-S"
+
+# available options: 
+#	-m	monitor on stdio|telnet
+#	-d 	freeze CPU at start
+#
+while [ $# -gt 0 ]; do
+	case "$1" in
+		 "-d")
+			DEBUG="-S"
+			;;
+		"-m")	shift
+			case $1 in
+				"s")	MONITOR="${MONITOR_STDIO}"
+					;;
+				"t")	MONITOR="${MONITOR_TELNET}"
+					;;
+				*)	echo "error: unknonw monitor option $1"
+					exit 1
+					;;
+			esac
+			;;
+		*)
+			echo "unknown option $1"
+			exit 1
+			;;
+	esac
+	shift
+
+done
+
+# use default
+if [ "z${MONITOR}" = "z" ]; then
+	MONITOR="${MONITOR_STDIO}"
 fi
 
-
-# raw
-# qemu-system-i386 -s ${DEBUG} ${MONITOR_STDIO} -curses -hda mios
-
-# GPT
-qemu-system-i386 -s ${DEBUG} ${MONITOR_STDIO} -curses -hda disk00.raw
+qemu-system-i386 -s ${DEBUG} ${MONITOR} -curses -hda disk00.raw
 
