25fc40eb969dca14c2758bed01273b1f12bed185
martin-0 <martin@bxlr.sk>
2022-05-06 15:34:49 +0200

    regs_t type

--- include/asm.h
+++ include/asm.h
@@ -44,6 +44,24 @@ struct trapframe {
 	uint32_t	eflags;
 } __attribute__((packed));
 
+typedef struct regs {
+	uint32_t	edi;
+	uint32_t	esi;
+	uint32_t	ebp;
+	uint32_t	esp;
+	uint32_t	ebx;
+	uint32_t	edx;
+	uint32_t	ecx;
+	uint32_t	eax;
+	uint16_t	ds;
+	uint16_t	es;
+	uint16_t	fs;
+	uint16_t	gs;
+	uint32_t	eip;
+	uint32_t	cs;
+	uint32_t	eflags;
+} __attribute__((packed)) regs_t;
+
 /*
  * https://gcc.gnu.org/onlinedocs/gcc/Machine-Constraints.html#Machine-Constraints
  *
