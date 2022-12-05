73448bb026014708cf3c10e64ed00b8b87a81254
martin-0 <martin@bxlr.sk>
2022-12-05 15:06:10 +0100

    one frame for both traps and interrupts

--- include/asm.h
+++ include/asm.h
@@ -9,21 +9,7 @@ typedef struct trap_desc {
 	int8_t	pushes_err:1;
 } __attribute__((packed)) trap_desc_t;
 
-struct irqframe {
-	uint32_t	edi;
-	uint32_t	esi;
-	uint32_t	ebp;
-	uint32_t	esp;
-	uint32_t	ebx;
-	uint32_t	edx;
-	uint32_t	ecx;
-	uint32_t	eax;
-	uint32_t	irq;
-	uint32_t	eip;
-	uint32_t	cs;
-	uint32_t	eflags;
-} __attribute__((packed));
-
+/* used for both trap and irq */
 struct trapframe {
 	uint32_t	edi;
 	uint32_t	esi;
@@ -37,8 +23,14 @@ struct trapframe {
 	uint16_t	es;
 	uint16_t	fs;
 	uint16_t	gs;
-	uint32_t	trapno;
-	uint32_t	err;
+	union {
+		uint32_t	trapnr;
+		uint32_t	irqdup;		/* duplicate: same as irq */
+	};
+	union {
+		uint32_t	err;
+		uint32_t	irq;
+	};
 	uint32_t	eip;
 	uint32_t	cs;
 	uint32_t	eflags;
