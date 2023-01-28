9387073c7ca323301cfc4a9a483359c3e3ff155d
martin-0 <martin@bxlr.sk>
2023-01-28 01:52:17 +0100

    add debug info

--- include/types.h
+++ include/types.h
@@ -5,35 +5,41 @@
 
 #define	RINGBUF32_SIZE		32
 
-#define	DEFINE_RING32(name, type)		\
-	struct { 				\
-		unsigned int ri:5;		\
-		unsigned int ci:5;		\
-		type buf[RINGBUF32_SIZE];	\
+#define	DEFINE_RING32(name, type)			\
+	struct { 					\
+		unsigned int ri:5; /* read index */	\
+		unsigned int fi:5; /* free index */	\
+		type buf[RINGBUF32_SIZE];		\
 	} name
 
-
 #ifdef DEBUG
-#define RING32_FETCH(rb) \
-	(rb)->buf[(rb)->ri]; \
-	do { \
-		printk("RING32_FETCH: ri: %d, val: 0x%x\n", (rb)->ri, (rb)->buf[(rb)->ri]); \
-		(rb)->ri++; \
-	} while (0)
+	#define RING32_FETCH(str, rb) \
+		(rb)->buf[(rb)->ri]; \
+		do { \
+			printk("RING32_FETCH: %s: ri: %d, val: 0x%x\n", (str), (rb)->ri, (rb)->buf[(rb)->ri]); \
+			(rb)->ri++; \
+		} while (0)
+
+	#define RING32_STORE(str, rb, item) do {						\
+			printk("RING32_STORE: %s: fi: %d, val: 0x%x\n", (str), (rb)->fi, item);	\
+			(rb)->buf[(rb)->fi++] = (item);					\
+		} while(0)
 #else
-#define	RING32_FETCH(rb)	(rb)->buf[(rb)->ri++]
+	#define	RING32_FETCH(rb)	(rb)->buf[(rb)->ri++]
+
+	#define RING32_STORE(rb, item) do {			\
+			(rb)->buf[(rb)->fi++] = (item);		\
+		} while(0)
 #endif
 
-#define RING32_STORE(rb, item) do {			\
-		(rb)->buf[(rb)->ci++] = (item);		\
-	} while(0)
+#define	RING32_DATA_READY(rb)	((rb)->ri != (rb)->fi)
 
-#define	RING32_DATA_READY(rb)	((rb)->ri != (rb)->ci) 
+#define	RING32_PEEK_FI(rb)		(&(rb)->buf[(rb)->fi])
+#define	RING32_PEEK_RI(rb)		(&(rb)->buf[(rb)->ri])
 
-#define	RING32_GET_FREE(rb)		(&(rb)->buf[(rb)->ci])
-#define	RIGG32_GET_UNREAD(rb)		(&(rb)->buf[(rb)->ri])
-
-#define	RING32_RI_ADVANCE(rb)		((rb)->ri++)
-#define	RING32_CI_ADVANCE(rb)		((rb)->ci++)
+#define	RING32_RI_ADD(rb)		((rb)->ri++)
+#define	RING32_RI_SUB(rb)		((rb)->ri--)
+#define	RING32_FI_ADD(rb)		((rb)->fi++)
+#define	RING32_FI_SUB(rb)		((rb)->fi--)
 
 #endif /* ifndef HAVE_TYPES_H */
